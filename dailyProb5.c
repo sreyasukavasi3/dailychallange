21f9d4bc1542120dc8cf6cd3287d8f3f6565b1fc
Sreya <33253396+sreyasukavasi3@users.noreply.github.com>
2018-07-25 20:26:55 +0530

    Add files via upload

---
+++ dailyProb5.c
@@ -0,0 +1,52 @@
+/*
+ * dailyProb5.c
+ * 
+ * Copyright 2018 Sreya <sreya@sreya-Inspiron-15-3567>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{   
+	int i,j,n,A[5][5];
+	for(i=0;i<5;i++) {
+		for(j=0;j<5;j++) {
+			scanf("%d",&A[i][j]);
+			
+		}}
+	
+	
+	
+		for(i=0;i<5;i++) {
+		for(j=0;j<5;j++) {
+			if(A[i][j]==1)
+			{ n = abs(2-i)+abs(2-j);
+			   	}	}
+				
+		              
+			
+		} printf("%d",n);		
+		           
+			return 0;
+}
+
+
